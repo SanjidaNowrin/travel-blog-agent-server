ecdfa0171f0c3ae1a7a4f8d52d0dc40f726e9d56
SanjidaNowrin <nowrin.cse18@gmail.com>
2023-02-23 12:25:14 +0600

    url

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uj11r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uj11r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
+const uri = process.env.MONGO_CONNECTION_STRING;
 console.log(uri);
 
 const client = new MongoClient(uri, {
