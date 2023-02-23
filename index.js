60a839633363184fd7944f6858b4268ad3829df6
SanjidaNowrin <nowrin.cse18@gmail.com>
2023-02-23 10:32:21 +0600

    database

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGO_URI;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uj11r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 console.log(uri);
 
